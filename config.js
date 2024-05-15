b731de9735adafe517e387f5fdd41b10c602f57d
awabofficial <awab.teto.official@gmail.com>
2024-05-15 07:48:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import fetch from 'node-fetch';
 import axios from 'axios';
 import moment from 'moment-timezone';
 
-global.botnumber = "249118215303"
+global.botnumber = ""
 global.confirmCode = ""
 
 global.owner = [
